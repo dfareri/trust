2d101297ae6479011c6b946037fbd9cd20dd3872
dfareri <dfareri@users.noreply.github.com>
2019-05-20 12:35:38 -0400

    amalia data final

--- analyzeTrust.m
+++ analyzeTrust.m
@@ -13,23 +13,23 @@ sublist = [101 102 103 104 105 106 107 108 109];
 
 
 for s = 1:length(sublist)
-    
+
     subj = sublist(s);
     runs = 3;
-    
+
     tmp_data = zeros(runs,10);
     for r = 1:runs
         fname = fullfile(maindir,'logs',num2str(subj),sprintf('sub-%02d_task-trust_run-%d_raw.csv',subj,r-1));
         fid = fopen(fname,'r');
         C = textscan(fid,[repmat('%f',1,14) '%s' repmat('%f',1,9)],'Delimiter',',','HeaderLines',1,'EmptyValue', NaN);
         fclose(fid);
-        
+
         RT = C{16}; % will do something with this later
-        % partner is 1 (computer), 2 (stranger), 3 (friend)
+        % partner is 1 (neu), 2 (neg), 3 (pos)
         Partner = C{4};
         trust_val = C{13}; % 0-8 (with '999' for no response)
         data = [Partner trust_val RT];
-        
+
         misses = length(find(data(:,2)==999));
         data(data(:,2)==999,:) = [];
         trust_val = data(:,2);
@@ -46,17 +46,15 @@ for s = 1:length(sublist)
         avg_stranger_RT = mean(RT(Partner==2,1));
         avg_friend_RT = mean(RT(Partner==3,1));
         tmp_data(r,:) = [avg_comp avg_stranger avg_friend avg_comp_logRT avg_stranger_logRT avg_friend_logRT avg_comp_RT avg_stranger_RT avg_friend_RT misses];
-        
-        
+
+
         % subnum,run,avg_comp,avg_stranger,avg_friend,pct_miss
         fprintf(fid_run,'%d,%d,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n',subj,r,tmp_data(r,:));
-        
+
     end
     % subnum,avg_comp,avg_stranger,avg_friend,pct_miss
     fprintf(fid_subj,'%d,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n',subj,mean(tmp_data));
-    
+
 end
 fclose(fid_subj);
 fclose(fid_run);
-
-
